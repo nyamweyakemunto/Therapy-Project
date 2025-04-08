2e24f957f274f044fa8ecc659fcd5c57ca5a6b8c
nyamweya kemunto <kemuntoisfly22@gmail.com>
2025-04-08 17:01:14 +0300

    patient progress

--- src/components/pages/patients/PatientProgressTracker.jsx
+++ src/components/pages/patients/PatientProgressTracker.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SideBar from '../../PatientSideBar';
 
 const PatientProgressTracker = () => {
   return (
-    <div>PatientProgressTracker</div>
+    <SideBar>
+        
+    </SideBar>
   )
 }
 
